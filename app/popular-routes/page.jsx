9e4771a717b0056d5633e2f318330eb6756010aa
Edwin Ko <edwin.mk.ko@gmail.com>
2024-05-06 08:46:38 -0700

    add route-details/[id] to see each individaul route for ratings

--- app/popular-routes/page.jsx
+++ app/popular-routes/page.jsx
@@ -1,5 +1,6 @@
 import Route from "@/components/Route";
 import fetchRoutes from "@/lib/data";
+import Link from "next/link";
 import React from "react";
 
 const PopularRoutesPage = async () => {
@@ -7,10 +8,14 @@ const PopularRoutesPage = async () => {
   return (
     <ul>
       {routes.map(route => {
-        return <Route key={route.id} name={route.route_name} description={route.description} />
+        return (
+          <Link href={`/route-details/${route.id}`}>
+            <Route key={route.id} name={route.route_name} description={route.description} />
+          </Link>
+        );
       })}
     </ul>
-  )
+  );
 }
 
 export default PopularRoutesPage;
---
+++ app/route-details/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RouteDetailsPage = ({params}) => {
+  return (
+    <div>{params.id}</div>
+  )
+}
+
+export default RouteDetailsPage;
