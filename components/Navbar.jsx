2293b92c9c2ff226edaf946d0a771003e23a214c
Racingwind <louis.k.hu@gmail.com>
2024-05-06 17:09:15 -0700

    navbar component that can be imported site wide

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from "@mui/material";
+import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon"
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar Component</h1>
-    </div>
+    <AppBar position="fixed">
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+          <CatchingPokemonIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ paddingRight: 2 }}>
+          My Cool App
+        </Typography>
+        <Stack direction="row" spacing={2} sx={{ flexGrow: 1 }} component="div">
+          <Button color="inherit">Popular Routes</Button>
+          <Button color="inherit">Popular Locations</Button>
+          <Button color="inherit">Dashboard</Button>
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">Account</Button>
+          <Button color="inherit">Logout</Button>        
+        </Stack>
+      </Toolbar>
+    </AppBar>
   )
 
 };
