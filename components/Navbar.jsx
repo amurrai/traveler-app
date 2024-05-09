8fd35ad30a43c80c5238222b7c8cbf735cea5ee5
Edwin Ko <edwin.mk.ko@gmail.com>
2024-05-08 23:21:24 -0700

    integrate session logic to navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,20 +12,31 @@ const Navbar = async () => {
   return (
     <AppBar position="fixed">
       <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+        <IconButton size="large" edge="start" color="inherit" aria-label="logo" href="/">
           <CatchingPokemonIcon />
         </IconButton>
         <Typography variant="h6" component="div" sx={{ paddingRight: 2 }}>
-          My Cool App
+          <Link href="/">My Cool App</Link>
         </Typography>
         <Stack direction="row" spacing={2} sx={{ flexGrow: 1 }} component="div">
-          <Button color="inherit">Popular Routes</Button>
+          <Button color="inherit" href="/popular-routes">Popular Routes</Button>
           <Button color="inherit">Popular Locations</Button>
-          <Button color="inherit">Dashboard</Button>
+          <Button color="inherit" href="/dashboard">Dashboard</Button>
         </Stack>
         <Stack direction="row" spacing={2}>
-          <Button color="inherit">Account</Button>
-          <Button color="inherit">Logout</Button>        
+          {session?.user ? (
+            <>
+              <Button color="inherit">Account</Button>
+              <LogoutButton />
+            </>
+          ) : (
+            <>
+              <Button color="inherit" href="/login">Login</Button>
+              <Button color="inherit" href="/register">Register</Button>
+            </>
+          )}
+          {/* <Button color="inherit">Account</Button>
+          <Button color="inherit">Logout</Button>         */}
         </Stack>
       </Toolbar>
     </AppBar>
@@ -33,17 +44,3 @@ const Navbar = async () => {
 };
 
 export default Navbar;
-
-{/* <div className=" bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10">
-  <div className="flex item-center justify-between">
-    <Link href="/">Home</Link>
-    {session?.user ? (
-      <LogoutButton />
-    ) : (
-      <div>
-        <Link href="/login">Login</Link> | 
-        <Link href="/register"> Register</Link>
-      </div>
-    )}
-  </div>
-</div> */}
