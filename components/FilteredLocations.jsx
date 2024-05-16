bb7782164ad47ac2ee6fec289da6c4781008e9f5
Lalika31253 <Lalika31253@gmail.com>
2024-05-15 18:42:10 -0600

    Add filtered locations component

---
+++ components/FilteredLocations.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { List, ListItem, ListItemText, Grid } from "@mui/material";
+
+const FilteredLocations = ({ filteredLocations }) => {
+  useEffect(() => {
+    console.log("Filtered Locations in useEffect:", filteredLocations);
+  }, [filteredLocations]);
+
+  console.log('FilterLocation Component sorted location:', filteredLocations);
+
+  return (
+    <Grid container spacing={3}>
+    <List sx={{ mt: 2 }}>
+      {Array.isArray(filteredLocations) && filteredLocations.length > 0 ? (
+        filteredLocations.map((location) => (
+          <ListItem key={location.id}>
+            <ListItemText
+              primary={`Name: ${location.name}`}
+              secondary={`City: ${location.city_name}, 
+                Description: ${location.description}, 
+                Category: ${location.category}, 
+                Rating: ${location.rating_id}`}
+            />
+          </ListItem>
+        ))
+      ) : (
+        <ListItem>
+          <ListItemText primary="No locations available" />
+        </ListItem>
+      )}
+    </List>
+    </Grid>
+  );
+};
+
+export default FilteredLocations;
