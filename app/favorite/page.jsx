2f4df497b7ee9be95697cc421b9c10eb130f4e8e
Lalika31253 <Lalika31253@gmail.com>
2024-05-17 16:14:17 -0600

    Add favorite page

---
+++ app/favorite/page.jsx
@@ -0,0 +1,51 @@
+
+import Location from "@/components/Location";
+import { fetchFilteredLocations, fetchLocation } from "@/lib/data";
+import Link from "next/link";
+import React from "react";
+import Filter from "@/components/Filter";
+import { Grid } from "@mui/material";
+
+
+
+const FavoritePage = async(props) =>  {
+
+  console.log(props);
+
+  const category = props.searchParams.category || '';
+  const minRating = props.searchParams.minRating || '';
+   const locations = await fetchFilteredLocations(category, minRating );
+
+ 
+ 
+  return (
+    <ul>
+      <Grid container spacing={3} sx={{ paddingTop: 10 }}>
+      <Grid item xs={12} md={3}>
+      <Filter minRating={minRating} selectedCategory={category}/>
+      </Grid>
+      <ul>
+      {locations.map(location => {
+        return (
+          <li key={location.id} >
+            <Link href={`/locations`}>
+              <Location location={location}  />
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+
+
+        </Grid>
+        
+    </ul>
+    
+  );
+};
+
+export default FavoritePage;
+
+
+
+
