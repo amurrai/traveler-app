ffaabd576c013f14f248069f6380440f83562e4a
Racingwind <louis.k.hu@gmail.com>
2024-05-17 01:38:09 -0700

    added paper component to wrap the login form for some elevation. centered the register message at the bottom for consistency

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { Box, Button, Container, Grid, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Paper, TextField, Typography } from "@mui/material";
 
 const LoginForm = () => {
 
@@ -39,45 +39,45 @@ const LoginForm = () => {
   };
 
   return (
-    <Container maxWidth="xs">
       <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="80vh">
-        <Typography component="h1" variant="h5" gutterBottom>Sign In</Typography>
-        <Box component="form" onSubmit={onSubmit} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}> 
-              <TextField variant="outlined" required fullWidth 
-                id="email"
-                label="Email Address"
-                name="email"
-                onChange={handleChange}
-              />
+        <Paper elevation={4} sx={{ padding: 2, height: '350px', minWidth: 'sx', backgroundColor: '#F0F5F9' }}>
+          <Typography component="h1" variant="h5" gutterBottom>Sign In</Typography>
+          <Box component="form" onSubmit={onSubmit} noValidate>
+            <Grid container spacing={2}>
+              <Grid item xs={12}> 
+                <TextField variant="outlined" required fullWidth 
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField variant="outlined" required fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  onChange={handleChange}
+                />
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField variant="outlined" required fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                onChange={handleChange}
-              />
-            </Grid>
-          </Grid>
-          <Box mt={3} textAlign="center">
-            <Button type="submit" fullWidth variant="contained" color="primary">
-              Login
-            </Button>
+            <Box mt={3} textAlign="center">
+              <Button type="submit" fullWidth variant="contained" color="primary">
+                Login
+              </Button>
+            </Box>
           </Box>
-        </Box>
           <div className="mx-auto my-4 flex w-full items-center justify-evenly
           before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 
           after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
             or
           </div>
-          <Typography>
+          <Typography sx={{ textAlign: 'center' }}>
             If you don't have an account, please <Link className="text-blue-500" href="/register">register</Link>
           </Typography>
+        </Paper>
       </Box>
-    </Container>
   )
 };
 
