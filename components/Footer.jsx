63b3f44a71ae00a8db9779a22378f54bd026a215
Racingwind <louis.k.hu@gmail.com>
2024-05-20 22:03:59 -0700

    centered the footer elements for better visual flow

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,19 @@
-import { AppBar, Toolbar, Typography, Stack, Button } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography, Stack, Button } from "@mui/material";
 
 const Footer = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ paddingRight: 2 }}>
-          WanderQuest
-        </Typography>
-        <Stack direction="row" spacing={2} sx={{ flexGrow: 1 }} component="div">
-          <Button color="inherit">About</Button>
-          <Button color="inherit">Contact us</Button>
-          <Button color="inherit">Who we are</Button>          
-        </Stack>
+        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+          <Typography variant="h6" component="div" sx={{ paddingRight: 2 }}>
+            WanderQuest
+          </Typography>
+          <Stack direction="row" spacing={2} component="div">
+            <Button color="inherit">About</Button>
+            <Button color="inherit">Contact us</Button>
+            <Button color="inherit">Who we are</Button>          
+          </Stack>
+        </Box>
       </Toolbar>
     </AppBar>
   )
