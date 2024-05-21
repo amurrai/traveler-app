8d23fe19c38f2c50d8cb9969334d7945d21db924
Racingwind <louis.k.hu@gmail.com>
2024-05-21 02:58:51 -0700

    added link to the favorites page

--- app/published/page.jsx
+++ app/published/page.jsx
@@ -99,7 +99,7 @@ const PublishedPage = () => {
       <Box display='flex' minWidth='200px' maxWidth='200px' flexDirection='column'>
         <Stack position='fixed' direction="column" spacing={2} component="div" padding={2}>
           <Button variant='outlined' href='/dashboard'>Your Routes</Button>
-          <Button variant='outlined'>Your Locations</Button>
+          <Button variant='outlined' href='/favorites'>Your Locations</Button>
           <Button variant='contained'>Published Routes</Button>
         </Stack>
       </Box>
